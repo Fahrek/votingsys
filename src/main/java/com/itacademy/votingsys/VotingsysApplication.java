174fb71c5e3ef8f46a74631172ee83247a6823ab
Andres G. Quina <andresgqjob@gmail.com>
2018-07-24 13:38:01 +0200

    votingSys
    a voting system app v0.0

---
+++ src/main/java/com/itacademy/votingsys/VotingsysApplication.java
@@ -0,0 +1,12 @@
+package com.itacademy.votingsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingsysApplication.class, args);
+	}
+}
